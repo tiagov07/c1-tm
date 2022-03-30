2a48a55c184c5d0cd8da60402accdb75638f86e0
tiagov07 <santiago.valencia@mercadolibre.com.co>
2022-03-30 16:53:48 -0500

    Ecommerce challenge done

--- practice3GoPointers/challenge2/Ecommerce.go
+++ practice3GoPointers/challenge2/Ecommerce.go
@@ -11,8 +11,23 @@
 // Borrar productos: recibe un usuario, borra los productos del usuario.
 package main
 
+import "fmt"
+
 func main() {
-	newproduct := product.newProduct("balon", 140000)
+	product1 := newProduct("balon", 140000)
+	product2 := product{"guantes", 90000, 1}
+
+	fmt.Printf(" first product created %+v \n", product1)
+	products := []product{product1}
+	newUser := User{"emilio", "casas", "emi@gmail.com", products}
+	fmt.Printf(" user created %+v \n", newUser)
+
+	newUser.addProduct("emilio", product2, 2)
+	fmt.Printf(" product added %+v \n", newUser)
+
+	newUser.delProducts("emilio")
+	fmt.Printf(" products deleted %+v \n", newUser)
+
 }
 
 type User struct {
@@ -24,20 +39,28 @@ type User struct {
 
 type product struct {
 	ProductName string
-	price       uint32
-	quantity    uint16
+	price       int
+	quantity    int
 }
 
-func (p product) newProduct(name string, price uint32) product {
-	newProduct := product{p.ProductName: name, p.price: price, p.quantity: 1}
+func newProduct(name string, price int) product {
+	newProduct := product{ProductName: name, price: price, quantity: 1}
 	return newProduct
 }
 
-func (u *User) addProduct(Name string, product product, quantity uint16) {
-	u.Name = Name
-	u.Products = append(product)
+func (u *User) addProduct(Name string, product product, quantity int) {
+	if u.Name == Name {
+		for i := 0; i < quantity; i++ {
+			product.quantity += 1
+		}
+		u.Products = append(u.Products, product)
+	}
+	fmt.Println(u)
 }
 
 func (u *User) delProducts(Name string) {
-	u.Products = (product)
+	if u.Name == Name {
+		u.Products = nil
+	}
+	fmt.Println(u)
 }
