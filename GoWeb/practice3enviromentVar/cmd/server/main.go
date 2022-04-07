7df3ef6b6ea136ab53e0559e24cfe3fa99edb6c6
tiagov07 <santiago.valencia@mercadolibre.com.co>
2022-04-07 08:05:31 -0500

    products API CRUD done

--- GoWeb/practice3enviromentVar/cmd/server/main.go
+++ GoWeb/practice3enviromentVar/cmd/server/main.go
@@ -1,7 +1,28 @@
 package main
 
-import "github.com/joho/godotenv"
+import (
+	"github/tiagov07/c1-tm/GoWeb/practice3enviromentVar/cmd/server/handler"
+	"github/tiagov07/c1-tm/GoWeb/practice3enviromentVar/internal/products"
+	"github/tiagov07/c1-tm/GoWeb/practice3enviromentVar/pkg/store"
+
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+)
 
 func main() {
 	_ = godotenv.Load()
+	db := store.New(store.FileType, "./products.json")
+	repoProducts := products.NewRepository(db)
+	serviceProducts := products.NewService(repoProducts)
+	product := handler.NewProduct(serviceProducts)
+	route := gin.Default()
+
+	productGroup := route.Group("/products")
+	productGroup.POST("/", product.Store())
+	productGroup.GET("/", product.GetAll())
+	productGroup.PUT("/:id", product.Update())
+	productGroup.PATCH("/:id", product.UpdateName())
+	productGroup.DELETE("/:id", product.Delete())
+	route.Run(":3000")
+
 }
